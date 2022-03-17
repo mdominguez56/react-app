ea637e8c9b1188422515729a08ab56fc80fc7c7f
Matias Dominguez Alonso <matiasdominguez3@gmail.com>
2022-03-16 21:52:58 -0300

    add test to 02-template file

---
+++ src/test/base-pruebas/02-template-string.test.js
@@ -0,0 +1,21 @@
+import "@testing-library/jest-dom";
+
+import { getSaludo } from "../../base-pruebas/02-template-string";
+
+describe("Pruebas en 02-template-strings", () => {
+  test("getSaludo debe de retornar Hola Matias", () => {
+    const nombre = "Matias";
+
+    const saludo = getSaludo(nombre);
+
+    expect(saludo).toBe("Hola " + nombre);
+  });
+
+  // getSaludo debe retornar Hola Carlos! si no hay argumento nombre
+  test("getSaludo debe retornar Hola Carlos! si no hay argumento nombre", () => {
+    const saludo = getSaludo();
+    expect(saludo).toBe("Hola Carlos");
+  });
+});
+
+// Seguir en video 53 de curso
