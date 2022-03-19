2c581ef8a72ebcb675ec88d208030b76b8186d72
Matias Dominguez Alonso <matiasdominguez3@gmail.com>
2022-03-18 22:39:58 -0300

    add test to 07-arr

---
+++ src/test/base-pruebas/07-deses-arr.test.js
@@ -0,0 +1,16 @@
+import "@testing-library/jest-dom";
+import { retornaArreglo } from "../../base-pruebas/07-deses-arr";
+
+describe("Pruebas en desestructuracion", () => {
+  test("Evaluar que sea un string", () => {
+    const [letras, numeros] = retornaArreglo();
+    expect(letras).toBe("ABC");
+    expect(typeof letras).toBe("string");
+  });
+
+  test("Evaluar que sea un numero", () => {
+    const [letras, numeros] = retornaArreglo();
+    expect(numeros).toBe(123);
+    expect(typeof numeros).toBe("number");
+  });
+});
