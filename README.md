b7d5727f601085a701e32e801b84b4a423589fee
Matias Dominguez Alonso <matiasdominguez3@gmail.com>
2022-03-19 01:17:17 -0300

    add test with enzyme

--- README.md
+++ README.md
@@ -5,5 +5,6 @@ Esta aplicacion de React contiene:
 - React JS
 - Javascript +ES6
 - Jest
+- Enzyme
 
 En construccion...
--- src/test/PrimeraApp.test.js
+++ src/test/PrimeraApp.test.js
@@ -1,13 +1,20 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import "@testing-library/jest-dom";
+// import { render } from "@testing-library/react";
 import PrimeraApp from "../PrimeraApp";
+import { shallow } from "enzyme";
 
 describe("Prueba en PrimeraApp component", () => {
-  test('Debe mostrar el mensaje, "Hola soy Goku"', () => {
+  // test('Debe mostrar el mensaje, "Hola soy Goku"', () => {
+  //  const saludo = "Hola soy Goku";
+  //  const { getByText } = render(<PrimeraApp saludo={saludo} />);
+  //  expect(getByText(saludo)).toBeInTheDocument();
+  //});
+
+  test("Debe demostrar el componente correctamente", () => {
     const saludo = "Hola soy Goku";
+    const wrapper = shallow(<PrimeraApp saludo={saludo} />);
 
-    const { getByText } = render(<PrimeraApp saludo={saludo} />);
-
-    expect(getByText(saludo)).toBeInTheDocument();
+    expect(wrapper).toMatchSnapshot();
   });
 });
